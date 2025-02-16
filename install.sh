add630f6fb7a9f85769bad300cd547cc69a15299
Robin Lux <studium@robinlux.de>
2025-02-16 13:20:39 +0100

    corrected sw.x and sd.x for assembly input

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 echo "compiling llvm and lld"
 BUILDDIR=build
 mkdir $BUILDDIR ; cd $BUILDDIR
-cmake -G Ninja -DCMAKE_BUILD_TYPE="MinSizeRel"  -DLLVM_USE_SPLIT_DWARF=True  -DCMAKE_INSTALL_PREFIX="../$BUILDDIR"  -DLLVM_OPTIMIZED_TABLEGEN=True  -DLLVM_BUILD_TESTS=False  -DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-linux-gnu" -DDEFAULT_SYSROOT="../../riscvio_gnu/riscv32-unknown-linux-gnu" -DLLVM_TARGETS_TO_BUILD="RISCV" -DLLVM_ENABLE_PROJECTS="clang;lld" ../llvm
-cmake --build .
+cmake -G Ninja -DCMAKE_BUILD_TYPE="MinSizeRel"  -DLLVM_USE_SPLIT_DWARF=True  -DCMAKE_INSTALL_PREFIX="../$BUILDDIR"  -DLLVM_OPTIMIZED_TABLEGEN=True  -DLLVM_BUILD_TESTS=False  -DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-linux-gnu"  -DLLVM_TARGETS_TO_BUILD="RISCV" -DLLVM_ENABLE_PROJECTS="clang;lld" ../llvm
+cmake --build . --target install
 
 echo "moving components to swkit"
-cp -rf bin ../../vio-swkit/tools/bin -p
+cp -rf bin ../../vio-swkit/tools/ -p
--- make_libc.sh
+++ make_libc.sh
@@ -3,9 +3,10 @@ C_COMPILER="/usr/local/llvm/bin/clang"
 CXX_COMPILER="/usr/local/llvm/bin/clang++"
 
 cd build
-cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_SPLIT_DWARF=True -DCMAKE_INSTALL_PREFIX="../install" \
+cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DLLVM_USE_SPLIT_DWARF=True -DCMAKE_INSTALL_PREFIX="../build" \
 -DLLVM_OPTIMIZED_TABLEGEN=True -DLLVM_BUILD_TESTS=False -DLLVM_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf" \
 -DLLVM_TARGETS_TO_BUILD="RISCV" \
 -DCMAKE_C_COMPILER=$C_COMPILER -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DLLVM_ENABLE_PROJECTS="clang;lld" ../llvm
 cmake --build . --target install
+cp -rf lib ../../vio-swkit/tools/ -p
 cd ..
